59c73ca38839f3533e8139ba99e57e7da101fb35
stranger <247480760@qq.com>
2023-05-09 23:57:58 +0800

    lesson1

---
+++ src/components/hello.vue
@@ -0,0 +1,11 @@
+<script lang='ts' setup>
+    const name = ref('isboyjc')
+</script>
+
+<template>
+    <div> hello {{name}}</div>
+</template>
+
+<style lang='scss' scoped>
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,28 @@
+<script lang='ts' setup>
+    const handleClickMini = () => {
+        AMessage.info('hello isboyjc, click mini AButton')
+    }
+</script>
+
+<template>
+    <div>hello isboyc, this is toolsdog home page</div>
+    <a-space>
+        <a-button type="primary" size="mini" @click="handleClickMini">Mini</a-button>
+        <a-button type="primary" size="small">Small</a-button>
+        <a-button type="primary">Medium</a-button>
+        <a-button type="primary" size="large">Large</a-button>
+    </a-space>
+    <Hello></Hello>
+    <icon-ep-alarm-clock class=""/>
+    <button class="btn-style">
+        Button
+    </button>
+</template>
+
+<style lang='scss' scoped>
+.btn-style{
+    @apply bg-blue-400 text-sm text-white font-mono font-light py-2 px-4 rounded border-2 border-blue-200;
+    @apply hover:bg-blue-500;
+    @apply dark:bg-blue-500 dark:hover:bg-blue-600;
+}
+</style>
